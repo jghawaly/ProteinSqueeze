6320e5d2575da95fe7a4df374e655aaf00c65039
James Ghawaly <jamesghawaly@gmail.com>
2018-12-12 03:01:27 +0000

    blag

--- cla.py
+++ cla.py
@@ -188,7 +188,7 @@ class AtlasClassifier:
 
 
 if __name__ == "__main__":
-    train_cifar10 = True
+    train_cifar10 = False
 
     if train_cifar10:
         with tf.Session() as sess:
@@ -203,7 +203,7 @@ if __name__ == "__main__":
             myTP.epochs = 25
             myTP.batch_size = 100
             myTP.learning_rate = 0.0001
-            myTP.dropout_rate = 0.1
+            myTP.dropout_rate = 0.05
             myTP.input_depth = 3
             myTP.input_width = 32
             myTP.input_height = 32
@@ -222,8 +222,8 @@ if __name__ == "__main__":
             myTP.testing_data = testing_data
             myTP.epochs = 25
             myTP.batch_size = 32
-            myTP.learning_rate = 0.0001
-            myTP.dropout_rate = 0.1
+            myTP.learning_rate = 0.00005
+            myTP.dropout_rate = 0.05
             myTP.input_depth = 1
             myTP.input_width = 512
             myTP.input_height = 512
